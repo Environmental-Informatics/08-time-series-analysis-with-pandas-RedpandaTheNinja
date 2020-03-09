bd3aa2ba11b7b60d58c629aad72d466431d35ef4
Kevin Lee <lee423@purdue.edu>
2020-03-09 17:15:14 -0400

    assign08

---
+++ PandasDatesDemo.py
@@ -0,0 +1,110 @@
+#KEvin Lee - assign 8 panda demo
+
+import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame, Panel
+pd.set_option('display.max_rows',15) # this limit maximum numbers of rows
+
+print("PANDA V:",pd.__version__)
+# !wget http://www.cpc.ncep.noaa.gov/products/precip/CWlink/daily_ao_index/monthly.ao.index.b50.current.ascii
+ao = np.loadtxt('monthly.ao.index.b50.current.ascii')
+print(ao[0:2])
+print(ao.shape)
+dates = pd.date_range('1950-01', periods=ao.shape[0], freq='M')
+print(dates)
+print(dates.shape)
+AO = Series(ao[:,2], index=dates)
+print(AO)
+
+import matplotlib.pyplot as plt
+# AO.plot()
+# plt.show()
+
+# AO['1980':'1990'].plot()
+# plt.show()
+
+# AO['1980-05':'1981-03'].plot()
+# plt.show()
+
+print(AO[120])
+
+print(AO['1960-01'])
+
+print(AO['1960'])
+
+print(AO[AO > 0])
+
+
+#Create Series the same way as we did for AO:
+nao = np.loadtxt('norm.nao.monthly.b5001.current.ascii')
+dates_nao = pd.date_range('1950-01', periods=nao.shape[0], freq='M')
+NAO = Series(nao[:,2], index=dates_nao)
+
+print(NAO.index)
+
+aonao = DataFrame({'AO' : AO, 'NAO' : NAO})
+# aonao.plot(subplots=True)
+# plt.show()
+
+print(aonao.head())
+print(aonao['NAO'])
+print(aonao.NAO)
+
+#diff
+aonao['Diff'] = aonao['AO'] - aonao['NAO']
+print(aonao.head())
+
+#removing col
+del aonao['Diff']
+print(aonao.tail())
+
+print(aonao['1981-01':'1981-03'])
+
+import datetime
+# aonao.loc[(aonao.AO > 0) & (aonao.NAO < 0) 
+#         & (aonao.index > datetime.datetime(1980,1,1)) 
+#         & (aonao.index < datetime.datetime(1989,1,1)),
+#         'NAO'].plot(kind='barh')
+# plt.show()
+
+############################
+#########STAT. ##############
+###########################
+print("Mean\n", aonao.mean())
+
+print("Max\n",aonao.max())
+
+print("Min\n", aonao.min())
+
+print("MMean\n", aonao.mean(1))
+
+print(aonao.describe())
+
+
+#rolling stat 
+AO_mm = AO.resample("A").mean()
+# AO_mm.plot(style='g--')
+# plt.show()
+
+AO_mm = AO.resample("A").median()
+# AO_mm.plot()
+# plt.show()
+
+AO_mm = AO.resample("3A").apply(np.max)
+# AO_mm.plot()
+# plt.show()
+
+AO_mm = AO.resample("A").apply(['mean', np.min, np.max])
+# AO_mm['1900':'2020'].plot(subplots=True)
+# AO_mm['1900':'2020'].plot()
+# plt.show()
+
+print(AO_mm)
+
+# aonao.rolling(window=12, center=False).mean().plot(style='-g')
+# plt.show()
+
+# aonao.AO.rolling(window=120).corr(other=aonao.NAO).plot(style='-g')
+# plt.show()
+
+print(aonao.corr())
---
+++ program-08.py
@@ -0,0 +1,78 @@
+#Kevin Lee assign 8
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy.random as random
+from datetime import datetime
+file = "WabashRiver_DailyDischarge_20150317-20160324.txt"
+data  = pd.read_csv(file,parse_dates=True, header=24, skiprows=[25], delimiter= '\t', index_col= ['datetime'])
+
+# agency_cd	site_no	datetime	tz_cd	01_00060	01_00060_cd
+#       agency_cd     site_no datetime tz_cd  01_00060 01_00060_cd
+# USGS    3335500  2015-03-17    00:00   EST     23200           P
+# USGS    3335500  2015-03-17    00:15   EST     23100           P
+# USGS    3335500  2015-03-17    00:30   EST     23100           P
+
+###################################
+#DAILY Average for the entire data#
+###################################
+precip_daily = data.resample('D').mean()
+# Create figure and plot space
+fig, ax = plt.subplots(figsize=(10, 10))
+
+# Add x-axis and y-axis
+ax.scatter(precip_daily.index.values,
+           precip_daily['01_00060'],
+           color='blue')
+
+# Set title and labels for axes
+ax.set(xlabel="Date",
+       ylabel="Discharge, cubic feet per second",
+       title="Daily Average ")
+plt.show()
+
+
+###################################
+#DAILY Average for top 10     data#
+###################################
+
+heightest=precip_daily.sort_values(by=['01_00060'], ascending=False)
+heightest =  heightest.iloc[0:10]
+
+# Create figure and plot space
+fig, ax = plt.subplots(figsize=(10, 10))
+
+# Add x-axis and y-axis
+ax.scatter(heightest.index.values,
+           heightest['01_00060'],
+           color='blue')
+
+# Set title and labels for axes
+ax.set(xlabel="Date",
+       ylabel="Discharge, cubic feet per second",
+       title="Daily Average top 10")
+plt.show()
+
+
+#####################################
+#Monthly Average for the entire data#
+#####################################
+
+# Resample to monthly precip sum and save as new dataframe
+precip_monthly = precip_daily.resample('M').mean()
+
+# Create figure and plot space
+fig, ax = plt.subplots(figsize=(10, 10))
+
+# Add x-axis and y-axis
+ax.scatter(precip_monthly.index.values,
+           precip_monthly['01_00060'],
+           color='blue')
+
+# Set title and labels for axes
+ax.set(xlabel="Date",
+       ylabel="Discharge, cubic feet per second",
+       title="Monthly Average ")
+
+plt.show()
